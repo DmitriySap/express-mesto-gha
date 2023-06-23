c35fb9dc16bd6ef55fa5f064e4062e291ea35960
Dmitriy Saprykin <saprykindima@bk.ru>
2023-06-23 17:07:49 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const userRoutes = require('./routes/userRouter');
+const cardRoutes = require('./routes/cardRouter');
+
+const { PORT = 3000 } = process.env;
+
+const app = express();
+app.use(bodyParser.json());
+mongoose.connect('mongodb://localhost:27017/mestodb')
+  .then(() => {
+    console.log('connected to db');
+  });
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: '6495a3dd913286db10cb00c8',
+  };
+
+  next();
+});
+
+app.use(userRoutes);
+app.use(cardRoutes);
+app.use('*', (req, res) => {
+  res.status(404).send({
+    message: 'Ничего не найдено, проверьте URL и метод запроса.',
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening at port ${PORT}`);
+});
