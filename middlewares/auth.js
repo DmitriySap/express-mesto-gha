556eb2bc6f5f3f7063125c9b626b4e9849043f64
Dmitriy Saprykin <saprykindima@bk.ru>
2023-07-05 08:24:28 +0300

    autotest fix

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,26 +1,20 @@
 const jwt = require('jsonwebtoken');
 
+const IncorrectTokenError = require('../utils/tokenIncorrectError');
+
 // eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
-
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return res
-      .status(401)
-      .send({ message: 'Not authorized' });
+    return next(new IncorrectTokenError('Необходима авторизация'));
   }
-
   const token = authorization.replace('Bearer ', '');
-
   let payload;
-
   try {
-    payload = jwt.verify(token, 'super-secret');
+    payload = jwt.verify(token, 'Yandex-the-best');
   } catch (err) {
-    return res.status(401).send({ message: 'Not authorized' });
+    return next(new IncorrectTokenError('Необходима авторизация'));
   }
-
-  // eslint-disable-next-line spaced-comment
-  req.user = payload; // запись пейлоуда в запрос
+  req.user = payload;
   next();
 };
